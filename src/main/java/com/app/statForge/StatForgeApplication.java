e6394a7c02d4ba4e8990d086ad432c8e5dc5c676
grafkust <grafkust@gmail.com>
2025-06-19 11:43:18 +0300

    Initial commit

---
+++ src/main/java/com/app/statForge/StatForgeApplication.java
@@ -0,0 +1,13 @@
+package com.app.statForge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatForgeApplication.class, args);
+	}
+
+}
