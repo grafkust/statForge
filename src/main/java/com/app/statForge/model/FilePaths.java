c2bc440f56d78681300fe70e8506d4d7e87b96db
grafkust <grafkust@gmail.com>
2025-06-28 20:12:06 +0300

    create save files logic

--- src/main/java/com/app/statForge/model/FilePaths.java
+++ src/main/java/com/app/statForge/model/FilePaths.java
@@ -1,12 +1,14 @@
 package com.app.statForge.model;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "file-path")
 @Getter
+@Setter
 public class FilePaths {
     private String losAngeles;
     private String newYork;
